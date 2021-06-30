d8ff3c7b40f1fdc60b8306190ef55db3a541f153
Pete Lower <pete.lower@gmail.com>
2021-06-29 19:47:59 -0700

    dynamically set state values on change and recalculate all values on load and input change

--- src/components/rpe/RPECalc.js
+++ src/components/rpe/RPECalc.js
@@ -1,12 +1,10 @@
 const RPECalc = {
 	cashflow: (income, expenses) => {
-		console.log('calcing now ', income, expenses);
 		return income - expenses;
 	},
 
 	cocroi: (totalInvestment, yearlyIncome) => {
-		return ( yearlyIncome / totalInvestment ) * 100;
-		// ( ( ( Gross Rent ) + ( Other Income) ) - ( Vacancy + Operating Expenses + Annual Mortgage Payments ) ) / Total Cash Invested
+		return ( yearlyIncome / totalInvestment ) * 100; // TODO: ( ( ( Gross Rent ) + ( Other Income) ) - ( Vacancy + Operating Expenses + Annual Mortgage Payments ) ) / Total Cash Invested
 	},
 
 	cap: (netOperatingIncome, PurchasePrice) => {
--- src/components/rpe/RentalPropertyEvaluator.js
+++ src/components/rpe/RentalPropertyEvaluator.js
@@ -20,48 +20,26 @@ class RentalPropertyEvaluator extends React.Component {
 			TotalExpenses: 20,
 		}
 		this.handleFieldChange = this.handleFieldChange.bind(this);
-		console.log('orig parent state ', this.state);
 	}
 
 	async handleFieldChange(inputChanged, newValue) {
-		console.log(inputChanged, newValue, 'we have changed')
-		if (inputChanged === 'RentPrice') {
-			await this.setState({
-				'CashFlow': ( newValue - ( newValue * ( .01 * parseInt( this.state.TotalExpenses ) ) ) ),
-			});
-			await this.setState({
-				'CashFlowYearly': ( this.state.CashFlow * 12 )
-			});
-		}
-		if (inputChanged === 'PurchasePrice') {
-			await this.setState({
-				'CoCROI': ( this.state.CashFlowYearly / ( this.state.PurchasePrice ) )
-			});
-		}
-		if (inputChanged === 'TotalExpenses') {
-			await this.setState({
-				'CashFlow': ( this.state.RentPrice - ( this.state.RentPrice * ( .01 * parseInt( newValue ) ) ) ),
-			});
-			await this.setState({
-				'CashFlowYearly': ( this.state.CashFlow * 12 )
-			});
-		}
-		console.log('parent state changed: ', this.state);
+
+		await this.setState({
+			[inputChanged]: parseInt( newValue )
+		});
+		this.calculateAll();
 	}
 
 	calculateAll() {
-		this.setState((prevState)=>{
-			console.log(prevState);
+		this.setState( ( prevState ) => {
 			const newState = { ...prevState };
 			newState.TotalCashInvested = newState.PurchasePrice + newState.ClosingCosts;
 			newState.CashFlowYearly = RPECalc.cashflow( ( newState.RentPrice * 12 ), ( ( newState.TotalExpenses * .01 ) * ( newState.RentPrice * 12 ) ) );
 			newState.CashFlow = newState.CashFlowYearly / 12;
 			newState.CoCROI = RPECalc.cocroi(newState.TotalCashInvested, newState.CashFlowYearly);
-			// newState.NetOperatingIncome = this.calculateNOI(newState.CashFlowYearly, newState.CashFlow);
 			newState.Cap = RPECalc.cap(newState.CashFlowYearly, newState.PurchasePrice);
 			return newState;
 		})
-		console.log('component mounted, running calcs', this.state);
 	}
 
 	componentDidMount() {
