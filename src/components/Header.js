d4f14b990e308d8009d06e6b9117e889dbbf2572
Pete Lower <pete.lower@gmail.com>
2021-06-29 20:22:24 -0700

    remove line breaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,9 +6,7 @@ const Header = () => {
 		<header className="App-header fixed top">
 			<img src={logo} className="App-logo" alt="logo" />
 			<h1 className="is-marginless">Rental Property Evaluator</h1>
-			<p className="is-marginless">
-			This is used to calculate numbers on a potential rental property.
-			</p>
+			<p className="is-marginless">This is used to calculate numbers on a potential rental property.</p>
 		</header>
 	);
 };
