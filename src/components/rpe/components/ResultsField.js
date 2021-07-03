07c791ebf795abd28e85760a939fc88c4a0c3701
Pete Lower <petelower@Petes-MacBook-Air.local>
2021-07-02 21:40:33 -0700

    add margin bottom class

--- src/components/rpe/components/ResultsField.js
+++ src/components/rpe/components/ResultsField.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ResultsField extends React.Component {
 	render() {
 		return(
-			<div className='flex space-between'>
+			<div className='flex space-between mb-half'>
 				<label className="is-size-1" id={`${this.props.fieldTitle}-label`} htmlFor={this.props.fieldTitle}>{this.props.fieldTitle}</label>
 				<span className="is-size-1" id={this.props.fieldTitle}>{ (this.props.result) ? this.props.result.toFixed(2) : this.props.result}{ ( this.props.isPercentage ) ? "%" : ""}</span>
 			</div>
