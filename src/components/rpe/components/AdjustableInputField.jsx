9c8204c677970efb181cb10f45816523a31d8d6f
Pete Lower <petelower@Petes-MacBook-Air.local>
2021-07-04 12:28:31 -0700

    feature/add-field-number-symbols: add dollar or percentage fields

--- src/components/rpe/components/AdjustableInputField.jsx
+++ src/components/rpe/components/AdjustableInputField.jsx
@@ -6,6 +6,8 @@ export default class AdjustableInputField extends React.Component {
 		console.log(this.props.curState.changeable[this.props.id]);
 		this.state = {value: (this.props.curState.changeable[this.props.id]) ? this.props.curState.changeable[this.props.id] : "0"};
 		this.childHandleFieldChange = this.childHandleFieldChange.bind(this);
+		this.inputBefore = ( this.props.numType === "currency" ) ? "$" : false;
+		this.inputAfter = ( this.props.numType === "percentage" ) ? "%" : false;
 	}
 	componentDidMount() {
 		document.getElementById(this.props.id).defaultValue = this.state.value;
@@ -15,9 +17,14 @@ export default class AdjustableInputField extends React.Component {
 	}
 	render() {
 		return(
-			<div className='formItem flex space-between'>
+			<div className='formItem flex space-between position-relative'>
 				<label className="is-size-1" htmlFor={this.props.id} id={`${this.props.id}-ariaLabel`} >{this.props.labelText}</label>
-				<input onChange={(e) => this.childHandleFieldChange(this.props.id,e.target.value)} type="text" id={this.props.id} name={this.props.id} aria-labelledby={`${this.props.id}-ariaLabel`} />
+				
+				<div className="position-relative">
+					<span className="position-absolute number-symbol dollar">{this.inputBefore}</span>
+					<input onChange={(e) => this.childHandleFieldChange(this.props.id,e.target.value)} type="text" id={this.props.id} name={this.props.id} aria-labelledby={`${this.props.id}-ariaLabel`} />
+					<span className="position-absolute number-symbol percentage">{this.inputAfter}</span>
+				</div>
 			</div>
 		)
 	};
