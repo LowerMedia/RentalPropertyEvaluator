d10e76bd2138bbd5bb2aad632f7b89ad401f35c6
9ete <pete@petelower.com>
2021-07-10 13:09:05 -0700

    (no branch, rebasing feature/EBITDA): feature/EBITDA: update formula in comment

--- src/components/rpe/RPECalc.js
+++ src/components/rpe/RPECalc.js
@@ -6,7 +6,7 @@ const RPECalc = {
 
 	EBDITA: (state) => {
 		return 99;
-		// totalIncomeYearly + totalTaxesPaidYearly + totalMortgageInterestPaidYearly + totalDeprectiationCostsYearly + totalAmoritizationCostsYearly
+		// state.calculated.TotalYearlyIncome + this.state.changeable.Taxes + totalMortgageInterestPaidYearly + totalDeprectiationCostsYearly + totalAmoritizationCostsYearly
 	},
 
 	CashFlow: (state) => {
