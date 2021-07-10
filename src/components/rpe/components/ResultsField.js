f724f6d6b978f14886dc20f468e664d734466be6
9ete <pete@petelower.com>
2021-07-10 12:34:45 -0700

    (no branch, rebasing feature/month-year-field-split): rebase stable and resolve conflicts, remove default vals

--- src/components/rpe/components/ResultsField.js
+++ src/components/rpe/components/ResultsField.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
+class Result extends React.Component {
+	constructor(props) {
+		super(props)
+		this.endSymbol = (this.props.isPercentage) ? "%" : "";
+		this.curVal = (this.props.yearly === 'true') ? this.props.resultValue * 12 : this.props.resultValue;
+	}
+	render() {
+		return (
+			<div className='column'><span  className="is-size-1" id={this.props.resultId}>{this.curVal.toFixed(2)} {this.endSymbol}</span></div>
+		)
+	}
+}
+
 export default class ResultsField extends React.Component {
+	constructor(props){
+		super(props)
+		this.addYearlyOutput = (this.props.fieldTimeBased) ? <Result yearly="true" resultId={this.props.fieldId} resultValue={this.props.result} isPercentage={this.props.isPercentage} /> : "";
+	}
 	render() {
 		return(
-			<div className='flex space-between mb-half'>
-				<label className="is-size-1" id={`${this.props.fieldTitle}-label`} htmlFor={this.props.fieldTitle}>{this.props.fieldTitle}</label>
-				<span className="is-size-1" id={this.props.fieldTitle}>{ (this.props.result) ? this.props.result.toFixed(2) : this.props.result}{ ( this.props.isPercentage ) ? "%" : ""}</span>
+			<div className='flex space-between mb-half results-column-container'>
+				<div className='column'><label className="is-size-1" id={`${this.props.fieldId}-label`} htmlFor={this.props.fieldTitle}>{this.props.fieldTitle}</label></div>
+				<Result yearly="false" resultId={this.props.fieldId} resultValue={this.props.result} isPercentage={this.props.isPercentage} />
+				{this.addYearlyOutput}
 			</div>
 		)
 	}
