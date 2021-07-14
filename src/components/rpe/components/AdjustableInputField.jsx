0bddca67646f1842da968534975c104813eeb773
9ete <pete@petelower.com>
2021-07-14 13:26:43 -0700

    fix/form-cur-value-to-match-state: correctly set input value based on current state vs onmount w/ JS

--- src/components/rpe/components/AdjustableInputField.jsx
+++ src/components/rpe/components/AdjustableInputField.jsx
@@ -3,16 +3,13 @@ import React from 'react';
 export default class AdjustableInputField extends React.Component {
 	constructor(props) {
 		super(props);
-		// console.log(this.props.curState.changeable[this.props.id]);
 		this.state = {value: (this.props.curState.changeable[this.props.id]) ? this.props.curState.changeable[this.props.id] : "0"};
 		this.childHandleFieldChange = this.childHandleFieldChange.bind(this);
 		this.inputBefore = ( this.props.numType === "currency" ) ? "$" : false;
 		this.inputAfter = ( this.props.numType === "percentage" ) ? "%" : false;
 	}
-	componentDidMount() {
-		document.getElementById(this.props.id).defaultValue = this.state.value;
-	}
 	async childHandleFieldChange(props, targetValue) {
+		this.setState({value:targetValue});
 		await this.props.handleFieldChange(props,targetValue);
 	}
 	render() {
@@ -22,7 +19,7 @@ export default class AdjustableInputField extends React.Component {
 				
 				<div className="position-relative">
 					<span className="position-absolute number-symbol dollar">{this.inputBefore}</span>
-					<input onChange={(e) => this.childHandleFieldChange(this.props.id,e.target.value)} type="text" id={this.props.id} name={this.props.id} aria-labelledby={`${this.props.id}-ariaLabel`} />
+					<input value={this.state.value} onChange={(e) => this.childHandleFieldChange(this.props.id,e.target.value)} type="text" id={this.props.id} name={this.props.id} aria-labelledby={`${this.props.id}-ariaLabel`} />
 					<span className="position-absolute number-symbol percentage">{this.inputAfter}</span>
 				</div>
 			</div>
