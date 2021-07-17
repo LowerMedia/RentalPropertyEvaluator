c69ac4ca07fb5ce0d59bf812546f5c4b870f5e6b
9ete <pete@petelower.com>
2021-07-17 14:40:08 -0700

    feature/add-calculated-figures-to-expenses: conditionally output before/after html to reduce props

--- src/components/rpe/components/AdjustableInputField.jsx
+++ src/components/rpe/components/AdjustableInputField.jsx
@@ -5,8 +5,6 @@ export default class AdjustableInputField extends React.Component {
 		super(props);
 		this.state = {value: (this.props.curState.changeable[this.props.id]) ? this.props.curState.changeable[this.props.id] : "0"};
 		this.childHandleFieldChange = this.childHandleFieldChange.bind(this);
-		this.inputBefore = ( this.props.numType === "currency" ) ? "$" : false;
-		this.inputAfter = ( this.props.numType === "percentage" ) ? "%" : false;
 	}
 	async childHandleFieldChange(props, targetValue) {
 		this.setState({value:targetValue});
@@ -18,9 +16,9 @@ export default class AdjustableInputField extends React.Component {
 				<label className="is-size-1" htmlFor={this.props.id} id={`${this.props.id}-ariaLabel`} >{this.props.labelText}</label>
 				
 				<div className="position-relative">
-					<span className="position-absolute number-symbol dollar">{this.inputBefore}</span>
+					{ this.props.numType === "currency" ? <span className="position-absolute number-symbol dollar">$</span> : null }
 					<input value={this.state.value} onChange={(e) => this.childHandleFieldChange(this.props.id,e.target.value)} type="text" id={this.props.id} name={this.props.id} aria-labelledby={`${this.props.id}-ariaLabel`} />
-					<span className="position-absolute number-symbol percentage">{this.inputAfter}</span>
+					{ this.props.numType === "percentage" ? <span className="position-absolute number-symbol percentage">%</span> : null }
 				</div>
 			</div>
 		)
