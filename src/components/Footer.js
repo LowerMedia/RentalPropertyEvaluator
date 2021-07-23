cee5fc09336b1b92eb504d7a0b3a3a9c7a9fc5d1
9ete <pete@petelower.com>
2021-07-23 16:06:46 -0700

    feature/add-bulma-continued: update header/footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Footer = () => {
 	return(
-		<footer className=" width-full navbar pb-0 mt-6">
+		<footer className=" width-full pb-0 mt-6">
 			<section className="container">
 				<h5 className="is-marginless has-text-right width-full mb-0">
+					<span className="is-size-6">This site was created with <i>React.js</i> & <i>Bulma</i> - </span>
 					<a
 					className="App-link is-size-6"
 					href="https://9ete.dev"
 					target="_blank"
 					rel="noopener noreferrer"
 					>
-						@9ete  This site was created with React.js & Bulma
+						@9ete
 					</a>
+					&nbsp;
 				</h5>
 			</section>
 		</footer>
