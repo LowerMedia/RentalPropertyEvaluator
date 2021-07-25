fcfa0eb7de3d256c03cfd48f18437dcc3ee899f7
Pete Lower <petelower@Petes-MacBook-Air.local>
2021-07-25 11:58:31 -0700

    update section names

--- src/components/rpe/RentalPropertyEvaluator.js
+++ src/components/rpe/RentalPropertyEvaluator.js
@@ -79,8 +79,8 @@ class RentalPropertyEvaluator extends React.Component {
 		return(
 			<section className="columns is-multiline container column width-full">
 				<section className="grid space-between flex-wrap columns container mr-0">
-					<FieldsSection PurchasePrice={this.state.changeable.PurchasePrice} sectionTitle={"Inputs"} handleFieldChange={this.handleFieldChange} curState={this.state} sectionId="RentalPropertyEvaluatorForm" fieldsArray={FieldDataObject.EvalFormFieldsArray} />
-					<FieldsSection PurchasePrice={this.state.changeable.PurchasePrice} sectionTitle={"Variable Expenses"} handleFieldChange={this.handleFieldChange} curState={this.state} sectionId="ExpenseSection" fieldsArray={FieldDataObject.ExpenseFormFieldsArray} />
+					<FieldsSection PurchasePrice={this.state.changeable.PurchasePrice} sectionTitle={"Income & Mortgage"} handleFieldChange={this.handleFieldChange} curState={this.state} sectionId="RentalPropertyEvaluatorForm" fieldsArray={FieldDataObject.EvalFormFieldsArray} />
+					<FieldsSection PurchasePrice={this.state.changeable.PurchasePrice} sectionTitle={"Expenses"} handleFieldChange={this.handleFieldChange} curState={this.state} sectionId="ExpenseSection" fieldsArray={FieldDataObject.ExpenseFormFieldsArray} />
 					<section className="FieldsSection side-padded width-one-fifth column py-0 is-4 resultsBox has-background-white">
 						<h3 className='left is-size-4 is-italic has-font-weight-bold title-border'>Results</h3>
 						{ FieldDataObject.ResultsBoxFields.map( (field,key) => <ResultsField key={key} isPassing={(field.threshold)?(this.state.calculated[field.id] > field.threshold)?"true":"false":null} result={(this.state.calculated[field.id]) ? this.state.calculated[field.id] : this.state[field.id]} toolTip={field.toolTip} fieldTitle={field.id} labelText={field.labelText} monthYear={field.monthYear} isPercentage={field.isPercentage} />) }
