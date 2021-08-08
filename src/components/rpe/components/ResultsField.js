c9165db6ef87fe39fd3c5132440ebac1752f3986
9ete <pete@petelower.com>
2021-08-07 19:15:33 -0700

    feature/currency-formatting: add currency formatting

--- src/components/rpe/components/ResultsField.js
+++ src/components/rpe/components/ResultsField.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
+function formatCurrency(num, isNotCurrency = false, isYearly = false) {
+	if ( isNotCurrency ) {
+		return num;
+	}
+	num = isYearly ? num * 12 : num;
+	let numArray = (""+num).split('.')[0].split('').reverse();
+	for (var i = numArray.length; i > 0; i--) {
+		var temp = i + 1;
+		if ( ! ( ( temp % 3 ) - 1) % 3 ) {
+			if (i != numArray.length) {
+				numArray.splice(i,0,',');
+			}
+		}
+	}
+	return numArray.reverse().join('') + "." + ( (""+num).split('.')[1] ? (""+num).split('.')[1][0] + "" + (""+num).split('.')[1][1] : "00" );
+}
+
 export default class ResultsField extends React.Component {
 	render() {
 		return(
@@ -8,8 +25,10 @@ export default class ResultsField extends React.Component {
 					{this.props.labelText}
 					<div className="tooltiptext">{this.props.toolTip}</div>
 				</label>
-				<span className="is-size-5" id={this.props.fieldTitle} data-testid={`${this.props.fieldTitle}`}>{ (this.props.result) ? this.props.result.toFixed(2) : this.props.result}{ ( this.props.isPercentage ) ? "%" : ""}</span>
-				{(this.props.monthYear) && <span className="is-size-5" id={`${this.props.fieldTitle}Yearly`} data-testid={`${this.props.fieldTitle}Yearly`}>{ (this.props.result) ? ( this.props.result * 12 ).toFixed(2): this.props.result * 12}{ ( this.props.isPercentage ) ? "%" : ""}</span>}
+				<span className="is-size-5" id={this.props.fieldTitle} data-testid={`${this.props.fieldTitle}`}>
+					{ (this.props.result) ? formatCurrency(this.props.result) : formatCurrency(this.props.result)}{ ( this.props.isPercentage ) ? "%" : ""}
+				</span>
+				{(this.props.monthYear) && <span className="is-size-5" id={`${this.props.fieldTitle}Yearly`} data-testid={`${this.props.fieldTitle}Yearly`}>{ (this.props.result) ? ( formatCurrency(this.props.result,this.props.isPercentage,true) ): formatCurrency(this.props.result,this.props.isPercentage,true)}{ ( this.props.isPercentage ) ? "%" : ""}</span>}
 			</div>
 		)
 	}
