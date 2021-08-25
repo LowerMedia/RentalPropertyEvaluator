d87d66caf544caafb4eb30e7267301f631359b22
9ete <pete@petelower.com>
2021-08-25 16:02:36 -0700

    feature/routing: move project meta info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,16 @@ const Footer = () => {
 		<footer className=" width-full pb-0 mt-6">
 			<section className="container">
 				<h5 className="is-marginless has-text-right width-full mb-0">
-					<span className="is-size-6">This site was created with <i>React.js</i> & <i>Bulma</i> - </span>
-					<a
+					<span className="is-size-7">This site was created with <i>React.js</i> & <i>Bulma</i> - Check it out on <a href='https://github.com/LowerMedia/rental-property-evaluator/'>Github</a></span>
+					
+					{/*<a
 					className="App-link is-size-6"
 					href="https://9ete.dev"
 					target="_blank"
 					rel="noopener noreferrer"
 					>
 						@9ete
-					</a>
+					</a>*/}
 					&nbsp;
 				</h5>
 			</section>
--- src/components/routes/Info.js
+++ src/components/routes/Info.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 function Info() {
-  return <section className='container py-6'>
-    <p>This site was created with React.js & Bulma - Check it out on <a href='https://github.com/LowerMedia/rental-property-evaluator/'>Github</a></p><br/>
-    
+  return <section className='container py-6'>  
     <h2 className='is-size-3 has-text-black'>Calculation List</h2>
     <p>
       <ul>
